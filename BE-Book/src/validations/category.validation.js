c24268763b7361324008d6027c1f95b2d991d582
Quanghwi <huydqph24532@fpt.edu.vn>
2023-11-01 11:39:26 -0700

    [BE] CRUD Category

---
+++ BE-Book/src/validations/category.validation.js
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+export const categoryValidation = new Joi.object({
+    name: Joi.string().required().messages({
+        'string.base': 'Tên phải alf dạng chuỗi',
+        'string.empty': 'Tên không được bỏ trống',
+        'string.min': `Tên phải có ít nhất  {#limit} kí tự`,
+        'any.required': 'Tên là bắt buộc',
+    }),
+});
