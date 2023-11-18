9ff1c2117b6b27141e4f0701a4902e4bd74d0bad
Quanghwi <huydqph24532@fpt.edu.vn>
2023-11-18 00:41:51 -0800

    [FE] Product Pages

--- FE-Book/src/app/component/pages/client/product-page/product-page.component.ts
+++ FE-Book/src/app/component/pages/client/product-page/product-page.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/component/interface/product';
+import { ProductService } from 'src/app/component/service/product/product.service';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css'],
 })
-export class ProductPageComponent {}
+export class ProductPageComponent {
+  status:boolean = false;
+  productName = '';
+  products:IProduct[]=[];
+  product!:IProduct;
+
+  constructor (private productService:ProductService){
+    this.productService.getAllProducts().subscribe(data=>{
+      this.products = data?.docs
+    })
+   }
+   getValue(e: any) {
+    this.productName = e.target.value;
+  }
+}
