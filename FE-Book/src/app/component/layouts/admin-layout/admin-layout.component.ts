81a7fac25f7f1834f8c7712b7ca77edcb5dfaf56
Quanghwi <huydqph24532@fpt.edu.vn>
2023-11-17 18:30:36 -0800

    [FE] Icon

--- FE-Book/src/app/component/layouts/admin-layout/admin-layout.component.ts
+++ FE-Book/src/app/component/layouts/admin-layout/admin-layout.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.css'],
 })
-export class AdminLayoutComponent {}
+export class AdminLayoutComponent {
+  @ViewChild('dropdownMenu') dropdownMenu!: ElementRef;
+  toggleDropdown() {
+    this.dropdownMenu.nativeElement.classList.toggle('show');
+  }
+
+  signOut() {
+    // Xử lý khi đăng xuất
+  }
+}
