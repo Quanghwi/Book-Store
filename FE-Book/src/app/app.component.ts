edec19abd523d2d1c8dc893b7ada4bbc578c9aad
Quanghwi <huydqph24532@fpt.edu.vn>
2023-11-19 00:20:30 -0800

    [FE] Home pages

--- FE-Book/src/app/app.component.ts
+++ FE-Book/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bookstore';
+  carouselImages = [
+    {
+      url: 'https://diamu.com.bd/wp-content/uploads/2022/09/Apple-iPhone-14-Pro-and-14-Pro-Max-4.jpg',
+      alt: 'Slide 1'
+    },
+    {
+      url: 'https://theme.hstatic.net/1000330442/1001030257/14/ms_banner_img8',
+      alt: 'Slide 2'
+    },
+    {
+      url: 'https://diamu.com.bd/wp-content/uploads/2022/09/Apple-iPhone-14-Pro-and-14-Pro-Max-4.jpg',
+      alt: 'Slide 3'
+    }
+  ]
 }
